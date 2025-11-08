4cb6f602da63e1220240957c6f6483f721f77192
wugipower <Z13720103910@163.com>
2025-11-08 23:55:38 +0800

    Add:utils plugin and utility classes for enhanced functionality

--- app/src/main/java/com/wz/ledcontrol/app/ble/WZBluetoothUtil.kt
+++ app/src/main/java/com/wz/ledcontrol/app/ble/WZBluetoothUtil.kt
@@ -1,4 +1,17 @@
 package com.wz.ledcontrol.app.ble
 
-class WZBluetoothUtil {
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+
+object WZBluetoothUtil {
+
+    fun isEnable(context: Context): Boolean {
+        return getBluetoothAdapter(context)?.isEnabled == true
+    }
+
+    fun getBluetoothAdapter(context: Context): BluetoothAdapter? {
+        val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager
+        return bluetoothManager?.adapter
+    }
 }
