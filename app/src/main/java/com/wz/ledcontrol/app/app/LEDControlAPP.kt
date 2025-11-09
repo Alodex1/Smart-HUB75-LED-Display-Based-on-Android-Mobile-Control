4f7cf831d12e7f437e93f639dc35858c4b464022
wugipower <Z13720103910@163.com>
2025-11-09 19:39:42 +0800

    Add:Implement LEDControlApp for application-wide initialization and setup

--- app/src/main/java/com/wz/ledcontrol/app/app/LEDControlAPP.kt
+++ app/src/main/java/com/wz/ledcontrol/app/app/LEDControlAPP.kt
@@ -1,6 +1,18 @@
-package com.wz.ledcontrol.app.app
+package com.vincent.android.myled.app
 
 import android.app.Application
+import com.tencent.mmkv.MMKV
+import com.vincent.android.myled.led.LEDController
+import com.vincent.android.myled.utils.ConfigManager
 
-class LEDControlAPP : Application() {
+class MyLEDApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // 初始化配置管理器
+        ConfigManager.init(this)
+
+        // 初始化LED控制器
+        LEDController.getInstance().initBLE(this)
+    }
 }
